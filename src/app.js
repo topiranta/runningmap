2f2d85fac3d91f925c30e4f954f1bbff24dd07b2
Topi Ranta <topi.ranta@gmail.com>
2020-12-02 15:09:27 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -50,5 +50,5 @@ var storage = multer.diskStorage({
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 });
